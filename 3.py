7e32e6ee8785dd4b59b1c8f89bc6b2e6480ec4b5
sem1312 <108529990+sem1312@users.noreply.github.com>
2024-04-15 15:26:50 -0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+str = input("ingrese una palabra: ")
+
+#primera letra
+print(str[0])
+#ultima
+print(str[len(str) - 1])
+#la del medio
+print(str[len(str)//2])
+#rebanada de la palabra
+print(str[1:len(str)-1])
